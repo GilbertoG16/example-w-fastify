291d8d1845e7872d48d9172385d6cf7af5692f30
Gilberto216 <gilberto.giron@utp.ac.pa>
2025-07-28 14:12:47 -0500

    AGREGANDO RUTA DE LOGIN

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,9 +2,11 @@ import { FastifyPluginAsync } from "fastify";
 
 // RUTAS
 import adminRoutes from "./admin";
+import publicRoutes from "./public";
 
 const allRoutes: FastifyPluginAsync = async (fastify) => {
     await fastify.register(adminRoutes, { prefix: "/admin" })
+    await fastify.register(publicRoutes, { prefix: "/public" })
 }
 
 export default allRoutes;
